1f14394e17681c0426bc4baa97b4f6f69188b233
PRASHANTSWAROOP001 <mrprashantswaroop@gmail.com>
2024-09-22 23:24:36 +0530

    Deploying The Project Add hashrouter

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/Travel-AI-Agent/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
