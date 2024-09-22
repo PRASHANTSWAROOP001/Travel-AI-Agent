7eb83bd650a3efd27641ab1e4ebe26ba8d253a02
PRASHANTSWAROOP001 <mrprashantswaroop@gmail.com>
2024-09-22 23:51:36 +0530

    Using Browser Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
-import { useState, lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Layout from './layout/Layout';
-import ErrorPage from './pages/ErrorPage';
-import HomePage from './pages/HomePage';
+import { useState, lazy, Suspense } from 'react'
+import {Button} from "./components/ui/button.jsx"
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import './App.css'
+import Layout from "./layout/Layout"
+import ErrorPage from "./pages/ErrorPage"
+import HomePage from "./pages/HomePage"
 
+const LoginPage = lazy(() => import("./pages/LoginPage"))
+const SavedTrip = lazy(() => import("./pages/SavedTrip"))
+const SearchPage = lazy(() => import("./pages/SearchPage"))
+const About = lazy(()=>import("./pages/About"))
+const PremiumSearch = lazy(()=>import("./pages/PremiumSearch.jsx"))
 
-const LoginPage = lazy(() => import('./pages/LoginPage'));
-const SavedTrip = lazy(() => import('./pages/SavedTrip'));
-const SearchPage = lazy(() => import('./pages/SearchPage'));
-const About = lazy(() => import('./pages/About'));
-const PremiumSearch = lazy(() => import('./pages/PremiumSearch.jsx'));
 
 function App() {
+
+  const router = createBrowserRouter([{
+    path:"/",
+    element: <Layout/>,
+    errorElement: <ErrorPage/>,
+    children:[
+      {
+        index:true,
+        element: <HomePage/>
+      },
+
+      {
+        path:"About",
+        element:(
+          <Suspense fallback={<div className='text-3xl text-gray-700 mt-10'> Loading... </div>}>
+            <About/>
+          </Suspense>
+        )
+      },
+
+      {
+        path:"Search",
+        element:(
+          <Suspense fallback={<div className='text-3xl text-gray-700 mt-10'> Loading... </div>}>
+            <SearchPage/>
+          </Suspense>
+        )
+      },
+      {
+        path:"saved-trip",
+        element:(
+          <Suspense fallback={<div className='text-3xl text-gray-700 mt-10'> Loading... </div>}>
+            <SavedTrip/>
+          </Suspense>
+        )
+      },
+
+      {
+        path:"login",
+        element:(
+          <Suspense fallback={<div className='text-3xl text-gray-700 mt-10'> Loading... </div>}>
+            <LoginPage/>
+          </Suspense>
+        )
+      },
+
+      {
+        path:"premium-search",
+        element:(
+          <Suspense fallback={<div className='text-3xl text-gray-700 mt-10'> Loading... </div>}>
+            <PremiumSearch/>
+          </Suspense>
+        )
+      },
+
+    ]
+  }])
+
   return (
-    <Router>
-      <Layout>
-        <Suspense fallback={<div className='text-3xl text-gray-700 mt-10'>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="about" element={<About />} />
-            <Route path="search" element={<SearchPage />} />
-            <Route path="saved-trip" element={<SavedTrip />} />
-            <Route path="login" element={<LoginPage />} />
-            <Route path="premium-search" element={<PremiumSearch />} />
-            <Route path="*" element={<ErrorPage />} /> {/* Catch all route for error handling */}
-          </Routes>
-        </Suspense>
-      </Layout>
-    </Router>
-  );
+   <RouterProvider router={router}/>
+  )
 }
 
-export default App;
+export default App
